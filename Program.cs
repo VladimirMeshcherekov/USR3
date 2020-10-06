7e3fe03825c86c1796916acc01ca756258a130e3
user <3gotosnapp@gmail.com>
2020-10-06 20:41:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string c;
+
+            Console.WriteLine("Введите температуру");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Укажите систему перевода");
+            c = Console.ReadLine();
+
+            if (c == "кельвин" | c == "к" | c == "k")
+            {
+                Console.WriteLine(a + 274);
+            }
+
+
+            if (c == "не кельвин" | c == "ф" | c == "f")
+            {
+                Console.WriteLine((a * 1.8) + 32);
+            }
+            else
+            {
+                Console.WriteLine("кажите правильно систему для перевода");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
